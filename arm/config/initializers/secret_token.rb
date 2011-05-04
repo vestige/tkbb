5c80eb1a49c4d4c1a79a5c1129e6a93b32f1cc32
vestige <vestige_sync@me.com>
2011-05-04 14:17:05 +0900

    new project

---
+++ arm/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppTkbb::Application.config.secret_token = 'b4e07a514b9e308429615393e3c68e8f64162e2a4cf463e4466648d198003115d128146ebbe043411672f1d4349ae14f449631407b9bfd9596095796aecb6177'
