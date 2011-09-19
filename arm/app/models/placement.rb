45acef16e4ec6be6fbbebe2367a4deab3b32f59b
vestige <vestige_sync@me.com>
2011-09-19 12:54:22 +0900

    #10 add base

--- arm/app/models/placement.rb
+++ arm/app/models/placement.rb
@@ -1,4 +1,8 @@
 class Placement < ActiveRecord::Base
+  attr_accessible :name, :latitude, :longitude
+  geocoded_by :name
+  after_validation :geocode, :if => :name_changed?
+  
   scope :search, lambda { |query|
     where([ "name LIKE ?", "%#{query}%" ])
   }
