3f19de112d66128155610c9d6bd097a72d98a76e
vestige <vestige_sync@me.com>
2011-10-26 07:51:40 +0900

    #14 delete log

--- arm/app/controllers/sessions_controller.rb
+++ arm/app/controllers/sessions_controller.rb
@@ -1,7 +1,5 @@
 class SessionsController < ApplicationController
-  def create
-    printf("aaaaa");
-    
+  def create    
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) ||
            User.create_with_omniauth(auth)
