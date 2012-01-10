bf206671ce2849efb7d017647d1f3ba45b235b69
GardenTree <takahiro.ueki@gmail.com>
2012-01-10 16:57:12 +0900

    #21 correct twitter authorization from environment variable

--- arm/config/initializers/omniauth.rb
+++ arm/config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, "CONSUMER_KEY", "CONSUMER_SECRET" # FIXME
+  provider :twitter, ENV["ARM_TWITTER_CONSUMER_KEY"], ENV["ARM_TWITTER_CONSUMER_SECRET"]
   #provider :facebook, "app_id", "app_secret" 
 end
