06d9a15f2e96d0742e701baf2f740c9338ee750f
GardenTree <takahiro.ueki@gmail.com>
2012-02-23 12:30:43 +0900

    #25 Fix bug about asset pipeline

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-AppTkbb::Application.config.session_store :cookie_store, :key => '_app_tkbb_session'
+AppTkbb::Application.config.session_store :cookie_store, key: '_app_tkbb_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
