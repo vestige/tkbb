95327a72b0ab6f0909152645940b3e0b0e1116ee
Naoto Takai <takai@recompile.net>
2012-08-12 09:08:13 +0900

    Add developer provider for omniauth

--- app/controllers/backdoor_controller.rb
+++
@@ -1,7 +0,0 @@
-class BackdoorController < ApplicationController
-  def login
-    session[:user_id] = params[:id]
-
-    redirect_to root_path
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,7 @@ AppTkbb::Application.routes.draw do
   root :to => "welcome#index"
   post "comment" => "welcome#comment"
   
-  match "/auth/:provider/callback" => "sessions#create"
-  match "/signout" => "sessions#destroy", :as => :signout
+  get "/auth/:provider/callback" => "sessions#create", :as => :signin
+  get "/signout" => "sessions#destroy", :as => :signout
 
-  #TODO Delete!!
-  namespace :backdoor do |backdoor|
-    match :login
-  end
 end
