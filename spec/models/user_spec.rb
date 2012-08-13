ac1e03808f9dd16f4124ecfce58a61bd6bf2445b
Naoto Takai <takai@recompile.net>
2012-08-13 22:27:21 +0900

    Simplify method with first_or_create

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe User do
-  describe 'create_with_omniauth' do
+  describe '.find_or_create_by_omniauth' do
+    subject { User.find_or_create_by_omniauth(auth) }
     let(:auth) do
       { 'provider'    => 'twitter',
         'uid'         => 'uid',
@@ -9,13 +10,24 @@ describe User do
         'credentials' => { 'token'  => 'token',
                            'secret' => 'secret' } }
     end
-    subject { User.create_with_omniauth(auth) }
 
-    it { should_not be_a_new(User) }
-    its(:provider) { should eq 'twitter' }
-    its(:uid) { should eq 'uid' }
-    its(:name) { should eq 'name' }
-    its(:token) { should eq 'token' }
-    its(:secret) { should eq 'secret' }
+    context 'find' do
+      before { create :user, :provider => 'twitter', :uid => 'uid', :name => 'name' }
+      it { should_not be_a_new(User) }
+
+      its(:provider) { should eq 'twitter' }
+      its(:uid) { should eq 'uid' }
+      its(:name) { should eq 'name' }
+    end
+
+    context 'create' do
+      it { should_not be_a_new(User) }
+
+      its(:provider) { should eq 'twitter' }
+      its(:uid) { should eq 'uid' }
+      its(:name) { should eq 'name' }
+      its(:token) { should eq 'token' }
+      its(:secret) { should eq 'secret' }
+    end
   end
 end
